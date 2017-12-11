92b6ed31eb52681e3a006673d3d865ecec2e8afd
Rocio Garcia <gooblyn@gmail.com>
2017-12-11 22:35:44 +0100

    puta ama

--- client/src/app/new-song-form/new-song-form.component.ts
+++ client/src/app/new-song-form/new-song-form.component.ts
@@ -14,6 +14,8 @@ export class NewSongFormComponent implements OnInit {
     artist:"",
     name:""
   }
+
+  tab;
   constructor(
     public auth: AuthService,
     public song: SongService,
@@ -26,8 +28,12 @@ export class NewSongFormComponent implements OnInit {
     if (name != "" && artist != ""){
       console.log(`Import the tab for ${artist} and ${name}`);
       this.song.search(artist, name)
-        .map(song => console.log(song))
-        .subscribe(() => (this.router.navigate(['/editTab','song._id']))) //aqui poner que te lleve a la pagina donde sale el tab
+        .map(song => {
+          this.tab = song.song;
+          console.log(song);
+        })
+        .subscribe();
+        // .subscribe(() => (this.router.navigate(['/editTab','song._id']))) //aqui poner que te lleve a la pagina donde sale el tab
     }
     else{
       console.log("You must provide an artist name and a song title");
