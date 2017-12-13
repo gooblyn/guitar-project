dd0cd7c0d73ae823878b8da82f1b0f65741fbc8a
Rocio Garcia <gooblyn@gmail.com>
2017-12-13 16:18:20 +0100

    Pedal CRUP done

--- client/src/app/pedal-details/pedal-details.component.ts
+++ client/src/app/pedal-details/pedal-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { PedalService } from '../services/pedal.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pedal-details',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedalDetailsComponent implements OnInit {
 
-  constructor() { }
+  pedal: any;
+  setPedal: [String];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private pedalServ: PedalService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getPedalDetails(params['id']);
+    });
   }
 
+  getPedalDetails(id) {
+    this.pedalServ.get(id)
+      .subscribe((pedal) => {
+        this.pedal = pedal;
+        this.setPedal = pedal.setArray;
+      });
+  }
+
+  deletePedal() {
+    this.pedalServ.delete(this.pedal._id)
+      .subscribe(() => {
+        this.router.navigate(['/pedalCollection']);
+        console.log("delete the pedal");
+      });
+  }
 }
