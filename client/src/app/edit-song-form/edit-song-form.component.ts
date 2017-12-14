1d1bed71fc497e8b1c280519328f8b72ae285a7f
Rocio Garcia <gooblyn@gmail.com>
2017-12-14 14:11:06 +0100

    prffff

--- client/src/app/edit-song-form/edit-song-form.component.ts
+++ client/src/app/edit-song-form/edit-song-form.component.ts
@@ -10,11 +10,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class EditSongFormComponent implements OnInit {
   artist;
-  title;
+  name;
   tab;
   guitAr;
   ampliAr;
   pedAr;
+  options:object={};
   song: any;
   formInfo = {
     artist:"",
@@ -24,6 +25,7 @@ export class EditSongFormComponent implements OnInit {
     ampli:"",
     pedal:[{}]
   };
+  numbers = ["1","2","3","4","5","6","7","8","9","10","11","12"];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -42,7 +44,7 @@ export class EditSongFormComponent implements OnInit {
       .subscribe((song) => {
         this.song = song;
         this.artist = song.song.artist;
-        this.title = song.song.name;
+        this.name = song.song.name;
         this.tab = song.song.textTab;
         this.guitAr = song.user.guitArray;
         this.ampliAr = song.user.ampliArray;
@@ -51,16 +53,39 @@ export class EditSongFormComponent implements OnInit {
   }
 
   submitForm(form){
-    console.log(form);
     this.formInfo.artist = form.value.artist;
-    this.formInfo.name = form.value.name;
+    this.formInfo.name = form.value.title;
     this.formInfo.texTab = form.value.tab;
     this.formInfo.guitar = form.value.guitar;
     this.formInfo.ampli = form.value.ampli;
-    for (let key in form.value.value) {
-      if (form.value.value[key]===true)
-        this.formInfo.pedal.push({pedal:key});
+
+    let objectCosas = [];
+    let array=[];
+    let prueba;
+    let arrayParaTrabajar = [];
+    for (let P in form.value){
+      if(P.includes('object')){
+        prueba = P.split(".");
+        if (form.value[P]=="")
+          prueba.push("12");
+        else
+          prueba.push(form.value[P]);
+        prueba.shift();
+        // console.log(prueba);
+      if (prueba.length > 0)
+        arrayParaTrabajar.push(prueba);
+      }
     }
+    let settingsToObject= [];
+    for (let x = 0; x < arrayParaTrabajar.length ; x++){
+      settingsToObject.push({ setting: arrayParaTrabajar[x][1],
+                              value: arrayParaTrabajar[x][2]});
+    }
+    objectCosas.push({
+      pedal:prueba[0],
+      settings: settingsToObject
+    })
+    this.formInfo.pedal = objectCosas;
     console.log(this.formInfo);
   }
 }
